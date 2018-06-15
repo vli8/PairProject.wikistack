c0367f53373abf74b83628a7aa3bdc19696752cd
Victor Li <vli8@fordham.edu>
2018-06-15 11:53:02 -0400

    Set up done

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require ('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const layout = require('./views/layout.js');
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static);
+app.use(morgan('dev'));//logging every req res
+app.use(express.static('/public'));//making file available 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));//convert the body content input into object useable in js
 
 app.get("/", (req,res,next)=>{
-    res.send('hello world');
-})
+    res.send(layout(''));
+});
 
 
 app.listen(3000);
